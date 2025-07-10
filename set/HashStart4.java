ab4c49656a0b2b634db4c8c4521bcb2dffdedb2f
kystic125 <120080427+kystic125@users.noreply.github.com>
2025-07-10 18:23:38 +0900

    컬렉션 프레임워크 - 해시(Hash)

---
+++ set/HashStart4.java
@@ -0,0 +1,36 @@
+package collection.set;
+
+import java.util.Arrays;
+
+public class HashStart4 {
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+        // 입력: 1, 2, 5, 8, 14, 99
+        Integer[] inputArray = new Integer[10];
+
+        add(inputArray, 1);
+        add(inputArray, 2);
+        add(inputArray, 5);
+        add(inputArray, 8);
+        add(inputArray, 14);
+        add(inputArray, 99);
+        System.out.println("inputArray = " + Arrays.toString(inputArray));
+
+        // 검색
+        int searchValue = 14;
+        int hashIndex = hashIndex(searchValue);
+        System.out.println("searchValue hashIndex = " + hashIndex);
+        Integer result = inputArray[hashIndex]; // O(1)
+        System.out.println(result);
+    }
+
+    private static void add(Integer[] inputArray, int value) {
+        int hashIndex = hashIndex(value);
+        inputArray[hashIndex] = value;
+    }
+
+    static int hashIndex(int value) {
+        return value % CAPACITY;
+    }
+}
